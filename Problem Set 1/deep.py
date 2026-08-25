812e53fe3bec4298b6431819ea37e2d0a51f3930
kazim.iqbal <iqkazim@gmail.com>
2026-08-25 18:59:18 +0200

    CS50 Problem Set 0

---
+++ Problem Set 1/deep.py
@@ -0,0 +1,5 @@
+deep = input ('What is the Answer to the Great Question of Life, the Universe and Everything?')
+if deep == '42' or deep == 'forty-two' or deep == 'Forty Two' or deep == 'forty two' or deep == 'FoRty TwO' or deep == ' 42 ':
+    print('Yes')
+else:
+    print('No')
---
+++ Problem Set 1/greeting.py
@@ -0,0 +1,7 @@
+greeting = input ('Greeting: ')
+if greeting.startswith ('Hello') or greeting.startswith ('hello'):
+    print ('$0')
+elif greeting.startswith ('H') or greeting.startswith ('h'):
+    print ('$20')
+else:
+    print ('$100')
