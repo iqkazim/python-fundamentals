0f706b9392df741a7f9e80e59b260f71265c402f
kazim.iqbal <iqkazim@gmail.com>
2026-08-25 19:00:27 +0200

    CS50 Problem Set 0

--- Problem Set 1/greeting.py
+++ Problem Set 1/greeting.py
@@ -1,5 +1,5 @@
 greeting = input ('Greeting: ')
-if greeting.startswith ('Hello') or greeting.startswith ('hello'):
+if greeting.startswith ('Hello') or greeting.startswith (' Hello ') or greeting.startswith ('hello'):
     print ('$0')
 elif greeting.startswith ('H') or greeting.startswith ('h'):
     print ('$20')
